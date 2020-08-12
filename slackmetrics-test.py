adae693c7e3a7aec25a915a43bcf95637e1a9c53
gcomlish <gregory@scribal.io>
2020-08-12 00:45:09 -0700

    initial commit...a working test of slackmetrics (auth and message in channel)

---
+++ slackmetrics-test.py
@@ -0,0 +1,15 @@
+from slack import WebClient
+from slackmetrics import SlackMetrics
+import os
+
+# Create a slack client
+slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN"))
+
+# Get a new StatsBot
+stats_bot = SlackMetrics("#random")
+
+# Get the onboarding message payload
+message = stats_bot.get_message_payload()
+
+# Post the onboarding message in Slack
+slack_web_client.chat_postMessage(**message)
---
+++ slackmetrics.py
@@ -0,0 +1,43 @@
+import random
+
+class SlackMetrics:
+
+    STATS_BLOCK = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "Sure! Flipping a coin....\n\n"
+            ),
+        },
+    }
+    
+    # The constructor for the class. It takes the channel name as the a
+    # parameter and sets it as an instance variable.
+    def __init__(self, channel):
+        self.channel = channel
+
+    #replace this method with kafka connector...
+
+    # Generate a random number to simulate flipping a coin. Then return the
+    # crafted slack payload with the coin flip message.
+    def _flip_coin(self):
+        rand_int =  random.randint(0,1)
+        if rand_int == 0:
+            results = "Heads"
+        else:
+            results = "Tails"
+
+        text = f"The result is {results}"
+
+        return {"type": "section", "text": {"type": "mrkdwn", "text": text}},
+
+    # Craft and return the entire message payload as a dictionary.
+    def get_message_payload(self):
+        return {
+            "channel": self.channel,
+            "blocks": [
+                self.STATS_BLOCK,
+                *self._flip_coin(),
+            ],
+        }
